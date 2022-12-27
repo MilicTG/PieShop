42e9e03fba758b5e429f40803c2c2aa29b64b11c
Ivan Milic <milic.tg@gmail.com>
2022-12-27 19:34:55 +0100

    Started with tutorial

---
+++ PieShop/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,18 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace PieShop.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee>? Employess { get; set; } = default;
+
+        protected override void OnInitialized()
+        {
+            Employess = MockDataService.Employees;
+        }
+    }
+}
+
+
+
