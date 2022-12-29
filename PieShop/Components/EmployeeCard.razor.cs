eb5909af8454411c6ed841d7d71995aec37259d6
Ivan Milic <milic.tg@gmail.com>
2022-12-29 20:58:05 +0100

    Employee card

--- PieShop/Components/EmployeeCard.razor.cs
+++ PieShop/Components/EmployeeCard.razor.cs
@@ -1,9 +1,11 @@
 ﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
 
 namespace PieShop.Components
 {
     public partial class EmployeeCard
     {
-        public Employee Employee { get; set; } = default;
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
     }
 }
