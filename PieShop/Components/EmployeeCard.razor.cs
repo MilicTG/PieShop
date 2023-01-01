0cf3794e910b92a06a3f3f7f58eb0c28532ea9f4
Ivan Milic <milic.tg@gmail.com>
2023-01-01 20:47:06 +0100

    Navigation

--- PieShop/Components/EmployeeCard.razor.cs
+++ PieShop/Components/EmployeeCard.razor.cs
@@ -10,5 +10,21 @@ namespace PieShop.Components
 
         [Parameter]
         public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (string.IsNullOrEmpty(Employee.LastName))
+            {
+                throw new Exception("Last name can't be empty");
+            }
+        }
+
+        public void NavigateToDetails(Employee selectedEmployee)
+        {
+            NavigationManager.NavigateTo($"/employeedetail/{selectedEmployee.EmployeeId}");
+        }
     }
 }
